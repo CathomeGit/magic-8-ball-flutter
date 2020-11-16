6482e4f940b62df2e020200a50f6d395cc736639
CathomeGit <cathome.daily.music@gmail.com>
2020-11-16 21:21:32 +0300

    74. Add a ball

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,28 @@ class BallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           backgroundColor: Colors.blue[900],
           title: Text('Ask Me Anything'),
         ),
-        body: Container(
-          color: Colors.blue,
-        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Image.asset('images/ball1.png'),
       ),
     );
   }
