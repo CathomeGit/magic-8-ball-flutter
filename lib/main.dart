c2caab22dba9b3acc8d0ed35a720c6429ec524b5
CathomeGit <cathome.daily.music@gmail.com>
2020-11-16 21:11:08 +0300

    73. Add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask Me Anything'),
+        ),
+        body: Container(
+          color: Colors.blue,
+        ),
       ),
     );
+  }
+}
