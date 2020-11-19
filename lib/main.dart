c9a37b0a395768a50c228f793a2961281716b016
CathomeGit <cathome.daily.music@gmail.com>
2020-11-19 19:18:04 +0300

    Restrict fortunetelling by date

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(
@@ -30,10 +31,10 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
-  final int threshold = 10;
+  final int threshold = 9;
   int counter = 0;
-  bool reachedLimit = false;
   int ballNumber = 1;
+  DateTime lockDate;
 
   @override
   Widget build(BuildContext context) {
@@ -42,22 +43,23 @@ class _BallState extends State<Ball> {
         child: FlatButton(
           onPressed: () {
             setState(() {
-              if (counter == 0) {
-                reachedLimit = false;
+              if (lockDate != null && !isToday(lockDate)) {
+                lockDate = null;
               }
               if (counter < threshold) {
                 ballNumber = Random().nextInt(4) + 1;
                 counter++;
               } else {
-                reachedLimit = true;
+                lockDate = DateTime.now();
                 counter = 0;
               }
             });
           },
-          child: !reachedLimit
+          child: lockDate == null
               ? Image.asset('images/ball$ballNumber.png')
               : Text(
-                  'TOO MANY QUESTIONS\nFOR TODAY!',
+                  'TOO MANY QUESTIONS\nTODAY (' +
+                      DateFormat('d.M.y').format(lockDate) + ')',
                   textAlign: TextAlign.center,
                   textScaleFactor: 1.5,
                   style: TextStyle(letterSpacing: 2, color: Colors.blue[50]),
@@ -65,5 +67,12 @@ class _BallState extends State<Ball> {
         ),
       ),
     );
+  }
+
+  // https://stackoverflow.com/a/60213219/4925022
+  bool isToday(DateTime date) {
+    final now = DateTime.now();
+    final diff = now.difference(date).inDays;
+    return diff == 0 && now.day == date.day;
   }
 }
